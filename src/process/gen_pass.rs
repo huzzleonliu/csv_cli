0749db4fc9527253c120497f85eb47c0cb80274a
huzz <huzzleonliu@foxmail.com>
2024-10-07 15:48:13 +0200

    checking password strenth

--- src/process/gen_pass.rs
+++ src/process/gen_pass.rs
@@ -1,4 +1,5 @@
 use rand::seq::SliceRandom;
+use zxcvbn::zxcvbn;
 
 const UPPER: &[u8] = b"ABCDEFGHJKLMNPQRSTUVWXYZ";
 const LOWER: &[u8] = b"abcdefghijkmnopqrstuvwxyz";
@@ -40,9 +41,12 @@ pub fn process_genpass(
 
     password.shuffle(&mut rng);
 
-    // TODO:make sure the password has at least one of each type of character
+    let password = String::from_utf8(password)?;
+    println!("{}", password);
 
-    println!("{}", String::from_utf8(password)?);
+    // output the password strength in std error
+    let estimate = zxcvbn(&password, &[]);
+    eprintln!("Password strength: {}", estimate.score());
 
     Ok(())
 }
