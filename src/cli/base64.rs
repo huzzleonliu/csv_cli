9960dde89bd9410f6671175f1f883de3fa0281a5
huzz <huzzleonliu@foxmail.com>
2024-11-01 12:12:06 +0100

    half way adding base64 function

--- src/cli/base64.rs
+++ src/cli/base64.rs
@@ -1,5 +1,7 @@
 use super::verify_input_file;
 use clap::Parser;
+use std::fmt;
+use std::str::FromStr;
 
 #[derive(Debug, Parser)]
 pub enum Base64SubCommand {
@@ -13,10 +15,51 @@ pub enum Base64SubCommand {
 pub struct Base64EncodeOpts {
     #[arg(short, long, value_parser = verify_input_file, default_value = "-")]
     pub input: String,
+    #[arg(short, value_parser = parse_base64_format, default_value = "standard")]
+    pub format: Base64Format,
 }
 
 #[derive(Debug, Parser)]
 pub struct Base64DecodeOpts {
     #[arg(short, long, value_parser = verify_input_file, default_value = "-")]
     pub input: String,
+    #[arg(short, value_parser = parse_base64_format, default_value = "standard")]
+    pub format: Base64Format,
+}
+
+#[derive(Debug, Clone, Copy)]
+pub enum Base64Format {
+    Standard,
+    UrlSafe,
+}
+
+fn parse_base64_format(format: &str) -> Result<Base64Format, anyhow::Error> {
+    format.parse()
+}
+
+impl FromStr for Base64Format {
+    type Err = anyhow::Error;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        match s {
+            "standard" => Ok(Base64Format::Standard),
+            "urlsafe" => Ok(Base64Format::UrlSafe),
+            _ => Err(anyhow::anyhow!("Invalid base64 format")),
+        }
+    }
+}
+
+impl From<Base64Format> for &'static str {
+    fn from(format: Base64Format) -> Self {
+        match format {
+            Base64Format::Standard => "standard",
+            Base64Format::UrlSafe => "urlsafe",
+        }
+    }
+}
+
+impl fmt::Display for Base64Format {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{}", Into::<&str>::into(*self))
+    }
 }
